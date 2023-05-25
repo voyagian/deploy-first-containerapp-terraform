1a0c280240897a9b48b56d24c40d95ed7072eae1
voyagian <98567340+voyagian@users.noreply.github.com>
2023-05-25 09:10:16 -1000

    fix sa in providers.tf

--- 3-deploy-terraform-base/terraform/providers.tf
+++ 3-deploy-terraform-base/terraform/providers.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "voyagian-deploy-first-containerapp-rg"
-    storage_account_name = "vgiandeployfirstcntnersa"
+    storage_account_name = "vgiandeployfirstcntersa"
     container_name       = "tfstate"
     key                  = "terraform-base.tfstate"
   }
